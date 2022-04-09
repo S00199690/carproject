cd8ab29a7d9875689897ce50bc85c404f07fc173
Ryan Mc Donald <S00199690@mail.itsligo.ie>
2022-04-09 03:38:43 +0100

    functions init

---
+++ functions/index.js
@@ -0,0 +1,80 @@
+const admin = require('firebase-admin');
+admin.initializeApp();
+const functions = require('firebase-functions');
+const cors = require('cors')({ origin: true });
+
+const db = admin.database().ref('/car-project');
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
+const getCarsFromDatabase = (res) => {
+    let cars = [];
+    return db.on(
+        'value',
+        snapshot => {
+            snapshot.forEach(car => {
+                cars.push({
+                    id: car.key,
+                    make: car.val().make,
+                    model: car.val().model,
+                    engine: car.val().engine,
+                    transmission: car.val().transmission
+                });
+            });
+            res.status(200).json(cars);
+        },
+        error => {
+            res.status(error.code).json({
+                message: `Error: ${error.message}`
+            });
+        }
+    );
+
+};
+
+exports.addCar = functions.https.onRequest((req, res) => {
+    return cors(req, res, () => {
+        if (req.method !== 'POST') {
+            return res.status(401).json({
+                message: 'Not allowed'
+            });
+        }
+        const make = req.query.make;
+        const model = req.query.model;
+        const engine = req.query.engine;
+        const transmission = req.query.transmission;
+        db.push({ make, model, engine, transmission });
+        getCarsFromDatabase(res);
+    });
+});
+
+exports.deleteCar = functions.https.onRequest((req, res) => {
+    return cors(req, res, () => {
+        if (req.method !== 'DELETE') {
+            return res.status(401).json({
+                message: 'Action not allowed'
+            })
+        }
+        const id = req.query.id;
+
+        db.child(id).remove();
+        getCarsFromDatabase(res);
+    });
+});
+
+exports.getCars = functions.https.onRequest((req, res) => {
+    return cors(req, res, () => {
+        if (req.method !== 'GET') {
+            return res.status(404).json({
+                message: 'Not allowed'
+            })
+        }
+        getCarsFromDatabase(res);
+    });
+});
---
+++ src/app/car.ts
@@ -0,0 +1,7 @@
+export interface car {
+    id:string;
+    make:string;
+    model:string;
+    engine:string;
+    transmission:string;
+}
